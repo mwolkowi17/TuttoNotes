eb6626cfc10ed32e47d064af14d4c33a999e3371
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-17 14:37:16 +0100

    Dodaj pliki projektów.

---
+++ TuttoNotes/TuttoNotes/Model/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuttoNotes.Model
+{
+    class Note
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime NoteDate { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
---
+++ TuttoNotes/TuttoNotes/Persistence/ISQLiteDb.cs
@@ -0,0 +1,10 @@
+﻿using SQLite;
+
+namespace TuttoNotes
+{
+    public interface ISQLiteDb
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
+
