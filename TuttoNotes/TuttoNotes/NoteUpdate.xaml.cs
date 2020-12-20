667083ee3caded80b7ae09e22fff65267533e030
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-20 13:37:37 +0100

    update note ready

--- TuttoNotes/TuttoNotes/NoteUpdate.xaml.cs
+++ TuttoNotes/TuttoNotes/NoteUpdate.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,25 @@ namespace TuttoNotes
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NoteUpdate : ContentPage
     {
+        public SQLiteAsyncConnection _connection;
         public NoteUpdate(Note note)
         {
             if (note == null)
                 throw new ArgumentNullException();
+            _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
             BindingContext = note;
             InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as Button;
+            var noteToUpdate = menuItem.CommandParameter as Note;
+            noteToUpdate.Title = NoteTilte.Text;
+            noteToUpdate.Body = NoteBody.Text;
+            noteToUpdate.NoteDate = NoteDate.Date;
+            await _connection.UpdateAsync(noteToUpdate);
+            await Navigation.PopAsync();
         }
     }
 }
